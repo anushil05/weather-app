86944eebe659e8e3bc65c9e5439a7ad294607a4e
anushil05 <markanushil@gmail.com>
2018-08-08 23:47:04 +0530

    style about and link to tools

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,7 +13,21 @@ var React = require('react');
 
 var About = (props) => {
     return (
-        <h3>About Component</h3>
+        <div>
+            <h1 className = "text-center">About Component</h1>
+            <p>This is a weather app build on react, I build this for fun.</p>
+            <p>
+            <ul>
+                <li>
+                    <a href = "https://github.com/facebook/react">React</a> - This was the JS framework. 
+                </li>
+                <li>
+                    <a href = "https://openweathermap.org">Open Weather Map</a> - This was the free API used to get the temperature.
+                </li>
+            </ul>
+            </p>
+        </div>
+        
     );
 } ;
 
